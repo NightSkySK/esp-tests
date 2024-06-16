8aab855f4720de295f6abf0e94850efbafcf3c8f
Szymon Klause <szymon.klause@gmail.com>
2024-06-16 16:24:16 +0200

    wiegand-test script to debug keypad comunication

--- wiegand-test/main/wiegand-test.c
+++ wiegand-test/main/wiegand-test.c
@@ -52,8 +52,8 @@ static void task(void *arg) {
   }
 
   // Initialize reader
-  ESP_ERROR_CHECK(wiegand_reader_init(&reader, CONFIG_EXAMPLE_D0_GPIO,
-                                      CONFIG_EXAMPLE_D1_GPIO, true,
+  ESP_ERROR_CHECK(wiegand_reader_init(&reader, CONFIG_KEYPAD1_D0_GPIO,
+                                      CONFIG_KEYPAD1_D1_GPIO, true,
                                       CONFIG_EXAMPLE_BUF_SIZE, reader_callback,
                                       WIEGAND_MSB_FIRST, WIEGAND_LSB_FIRST));
 
